100a1c1f79863393978d587db54680782588ab65
lkw1996 <lkw3703@gmail.com>
2022-08-15 16:20:37 +0900

    ADD : code from the previous project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    {/* <Login /> */}
+    <Main />
   </React.StrictMode>
 );
