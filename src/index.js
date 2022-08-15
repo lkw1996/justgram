a90e816838b8ea138a14066e4d99e79f4df82997
lkw1996 <lkw3703@gmail.com>
2022-08-15 16:25:30 +0900

    Remove & Update : unnecessary code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
-import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Login /> */}
-    <Main />
+    <Login />
+    {/* <Main /> */}
   </React.StrictMode>
 );
