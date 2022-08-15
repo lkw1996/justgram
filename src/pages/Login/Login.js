38c1c21c86660f1c426ff5e1f93a2662b4fd5b95
lkw1996 <lkw3703@gmail.com>
2022-08-15 16:08:22 +0900

    ADD : code from the previous project

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,41 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="flex-center container">
+      <div className="login-container">
+        <div className="logo-wrapper flex-center">
+          <span className="logo-text">Justgram</span>
+        </div>
+        <div className="form-wrapper">
+          <form id="login-form">
+            <div className="flex-center login-wrapper">
+              <input
+                id="id-input"
+                className="login-input"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+              />
+            </div>
+            <div className="flex-center login-wrapper">
+              <input
+                id="pw-input"
+                className="login-input"
+                placeholder="비밀번호"
+              />
+            </div>
+            <div className="flex-center button-wrapper">
+              <button id="login-btn" className="login-button" type="submit">
+                로그인
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="flex-center extra-wrapper">
+          <a className="find-password">비밀번호를 잊으셨나요?</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
