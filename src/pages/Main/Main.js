baaff4cfdbfba855b6c6741cb87dbae82fdd4247
lkw1996 <lkw3703@gmail.com>
2022-08-17 16:02:20 +0900

    ADD : 댓글 기능 (update user comment using state)

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "./Main.scss";
 
 function Main() {
+  const [commentList, setCommentList] = useState([]);
+  const [comment, setComment] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setCommentList([...commentList, comment]);
+  };
+
+  const handleInput = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
     <>
       <div className="to-do header-wrapper">
@@ -49,78 +62,21 @@ function Main() {
               <div className="feed-comment-like padding-10">
                 ainworld님 외 10명이 좋아합니다.
               </div>
-              <div className="feed-comment-list padding-10"></div>
+              <div className="feed-comment-list padding-10">
+                {commentList.map((comment) => {
+                  return <li>{comment}</li>;
+                })}
+              </div>
               <div>42분전</div>
               <div className="feed-comment-write padding-10">
                 <input
                   className="comment-write-input"
                   placeholder="댓글달기..."
+                  onChange={handleInput}
                 />
-                <button className="comment-write-button">게시</button>
-              </div>
-            </div>
-          </div>
-          <div className="feed-container">
-            <div className="to-do feed-header padding-10">
-              <div className="vertical-center">
-                <img alt="이미지" /> 아이디
-              </div>
-              <div>...</div>
-            </div>
-            <div className="to-do feed-image">
-              <img
-                className="feed-img"
-                src="https://images.unsplash.com/photo-1659434322358-72fd6d1e74ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80"
-              />
-            </div>
-            <div className="to-do feed-menu padding-10">
-              <div>왼쪽 아이콘</div>
-              <div>오른쪽 아이콘</div>
-            </div>
-            <div className="to-do feed-comment">
-              <div className="feed-comment-like padding-10">
-                ainworld님 외 10명이 좋아합니다.
-              </div>
-              <div className="feed-comment-list padding-10"></div>
-              <div>42분전</div>
-              <div className="feed-comment-write padding-10">
-                <input
-                  className="comment-write-input"
-                  placeholder="댓글달기..."
-                />
-                <button className="comment-write-button">게시</button>
-              </div>
-            </div>
-          </div>
-          <div className="feed-container">
-            <div className="to-do feed-header padding-10">
-              <div className="vertical-center">
-                <img alt="이미지" /> 아이디
-              </div>
-              <div>...</div>
-            </div>
-            <div className="to-do feed-image">
-              <img
-                className="feed-img"
-                src="https://images.unsplash.com/photo-1659434322358-72fd6d1e74ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80"
-              />
-            </div>
-            <div className="to-do feed-menu padding-10">
-              <div>왼쪽 아이콘</div>
-              <div>오른쪽 아이콘</div>
-            </div>
-            <div className="to-do feed-comment">
-              <div className="feed-comment-like padding-10">
-                ainworld님 외 10명이 좋아합니다.
-              </div>
-              <div className="feed-comment-list padding-10"></div>
-              <div>42분전</div>
-              <div className="feed-comment-write padding-10">
-                <input
-                  className="comment-write-input"
-                  placeholder="댓글달기..."
-                />
-                <button className="comment-write-button">게시</button>
+                <button className="comment-write-button" onClick={handleClick}>
+                  게시
+                </button>
               </div>
             </div>
           </div>
